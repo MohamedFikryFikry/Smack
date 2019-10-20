a645e866ccce4d1bc0a8075990776f316926c971
MOHAMED <mohamed@MOHAMEDs-iMac.local>
2019-10-20 14:06:20 +0200

    SwReveal make menu

---
+++ Smack/Smack/controller/ChatVc.swift
@@ -0,0 +1,28 @@
+//
+//  ChatVc.swift
+//  Smack
+//
+//  Created by MOHAMED on 10/20/19.
+//  Copyright © 2019 MOHAMED. All rights reserved.
+//
+
+import UIKit
+
+class ChatVc: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+    }
+    
+   
+   
+
+}
